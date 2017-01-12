7698ed5dfa660e7248b9cc59b7680c29133a094b
etaranov <etaranov@yandex-team.ru>
2017-01-12 16:33:36 +0300

    complicated search

--- TcVisibleProjectsBehavior.js
+++ TcVisibleProjectsBehavior.js
@@ -4,7 +4,7 @@
 
 "use strict";
 
-(function (Polymer, Ajax, _, Map) {
+(function (Polymer, Ajax, _) {
     var Errors = {
         COMMUNICATION_ERROR: _.template('Server "<%= url %>" returned status <%= status %>'),
         CAN_NOT_PARSE_RESPONSE: 'Error while parse response'
@@ -48,8 +48,8 @@
             });
         },
 
-        /** @type {Map} */
-        _projectsMap: null,
+        /** @type {Array} */
+        _projects: null,
 
         /** @type {Map} */
         _filteredProjectsMap: null,
@@ -59,17 +59,19 @@
 
         /**
          * @param rawProjects {Array<{ id:String, parentProjectId:String, name:String }>}
-         * @return {Map}
+         * @return {Array}
          */
         _parseProjects: function (rawProjects) {
-            var resultMap = new Map();
+            var result = [];
+            result._index = {};
 
             // Handle Root project
             var _project = rawProjects[0];
             _project._level = 0;
             _project._fullName = '';
-
-            resultMap.set(_project.id, _project);
+            _project._children = [];
+            result.push(_project);
+            result._index[ _project.id ] = _project;
 
             // Save root project ID
             this._rootProject = _project;
@@ -77,97 +79,97 @@
             var _parent;
             for (var i = 1/* Omit root project */, len = rawProjects.length; i < len; i++ ) {
                 _project = rawProjects[i];
-                _parent = resultMap.get(_project.parentProjectId);
+                _parent = result._index[ _project.parentProjectId ];
+
+                _project._children = [];
                 _project._level = _parent._level + 1;
                 _project._fullName = _parent._fullName + '::' + _project.name.toLowerCase();
+                result._index[ _project.id ] = _project;
 
-                resultMap.set(_project.id, _project);
+                _parent._children.push(_project);
             }
 
-            return resultMap;
+            return result;
+        },
+
+        /**
+         * @param project {Object}
+         * @return {Array<String>}
+         */
+        _getProjectNodes: function (project) {
+            var html = [ this._ioGetProjectHTML(project) ];
+
+            if (project._children.length) {
+                var self = this;
+                project._children.forEach(function (_project) {
+                    html.push.apply(html, self._getProjectNodes(_project));
+                })
+            }
+
+            return html;
         },
 
         /**
          * @param filter {String}
          * @param [isProgressive] {Boolean} true to filter over current _filteredProjectsMap
-         * @return {Array<Strings>} valid HTML strings elements
+         * @return {Object} map of visible projects
          */
-        _filterProjectsNodes: function (filter, isProgressive) {
-            var projectsMap = isProgressive
-                ? this._filteredProjectsMap || this._projectsMap
-                : this._projectsMap;
-
-            if (! projectsMap.size) {
-                return [];
+        _getFilteredProject: function (filter, isProgressive) {
+            if (! this._projects || ! this._projects.length) {
+                return {};
             }
 
-            var projectsArr = Array.from(projectsMap.values());
-            var filteredProjects = [];
-            var nodes = [];
-            var _project, _projectIndex, _parent, _child; // Cursors
-
-            // Fastest way for empty filter only
+            // Fastest for empty filter only
             if (! filter.length) {
-                for (var i = 1 /* Omit root project */, len = projectsArr.length; i < len; i++) {
-                    _project = projectsArr[i];
-                    nodes.push(this._ioGetProjectHTML(projectsArr[i]));
-                }
+                return this._projects._index;
             }
 
-            // For defined filter
-            else {
-                // Mark Root as already displayed
-                filteredProjects = [[this._rootProject.id, this._rootProject]];
-                filteredProjects[this._rootProject.id] = true;
+            return this._filterProject(this._rootProject, filter, {});
+        },
 
+        _filterProject: function (project, filter, filtered) {
+            var match = false;
+
+            if (! filter) {
+                filtered[ project.id ] = true;
+            } else {
                 var filterParts = filter.split(' ');
                 var regexp = new RegExp('(' + filterParts.map(_.escapeRegExp).join(').+(') + ')', 'i');
 
-                for (var j = 1 /* Omit root project */, len = projectsArr.length; j < len; j++) {
-                    _project = projectsArr[j];
+                if (regexp.test(project._fullName)) {
+                    match = true;
 
-                    if (regexp.test(_project._fullName)) {
-                        _projectIndex = nodes.length;
-
-                        // Display current project
-                        nodes.push(this._ioGetProjectHTML(_project, filterParts));
-                        filteredProjects.push([_project.id, _project]);
-                        filteredProjects[_project.id] = true;
-
-                        // Display all parents
-                        _parent = projectsMap.get(_project.parentProjectId);
-                        while (! filteredProjects[_parent.id]) {
-                            nodes.splice(_projectIndex, 0, this._ioGetProjectHTML(_parent, filterParts));
-                            filteredProjects.splice(_projectIndex, 0, [_parent.id, _parent]);
-                            filteredProjects[_parent.id] = true;
-                            _parent = projectsMap.get(_parent.parentProjectId);
-                        }
-                        _parent = null;
-
-                        // Display all children
-                        _child = projectsArr[j + 1];
-                        while (_child && _child._level > _project._level) {
-                            nodes.push(this._ioGetProjectHTML(_child, filterParts));
-                            filteredProjects.push([_child.id, _child]);
-                            filteredProjects[_child.id] = true;
-                            j++;
-                            _child = projectsArr[j + 1];
-                        }
-                        _child = null;
+                    // Display all parents
+                    var _parent = this._projects._index[ project.parentProjectId ];
+                    while (_parent !== this._rootProject && !filtered[_parent.id]) {
+                        filtered[ _parent.id ] = true;
+                        _parent = this._projects._index[ _parent.parentProjectId ];
                     }
-                }
+                    _parent = null;
 
-                this._filteredProjectsMap = new Map(filteredProjects);
+                    // Display current project
+                    filtered[ project.id ] = true;
+                }
             }
 
-            return nodes;
+            // Iterate children
+            if (project._children.length) {
+                for (var i = 0, len = project._children.length; i < len; i++) {
+                    this._filterProject(project._children[i], match ? null : filter, filtered);
+                }
+            }
+
+            return filtered;
         },
 
         /**
          * @param result {{count: Number, href: String, project: Array}}
          */
         _onProjectsLoaded: function (projects) {
-            this._projectsMap = this._parseProjects(projects);
+            var ts = (new Date()).getTime();
+            this._projects = this._parseProjects(projects);
+            this._ioRenderHiddenProjects(this._getProjectNodes(this._rootProject));
+            console.log('Initial render time: ', (new Date()).getTime() - ts);
             this._ioApplyCurrentFilter();
         },
 
@@ -178,4 +180,4 @@
             this._ioShowError(error.message, 'LOAD_PROJECTS_ERROR');
         }
     };
-})(window.Polymer || {}, window.Ajax, window._, window.Map);
+})(window.Polymer || {}, window.Ajax, window._);
